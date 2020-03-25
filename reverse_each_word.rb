85de27c0279ab2eaf64e7a9888956ab91b02b941
Arynn Boniface <arynn.boniface@gmail.com>
2020-03-25 00:08:41 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -2,6 +2,8 @@ def reverse_each_word(string)
   reverse = []
   string = string.split(" ")
   string.each do |word|
-    reverse << word.reverse
+    word = word.reverse
+    reverse << word
   end
+    return reverse.join(" ")
 end
